d896fc935c8262bb38296ddd54c82ba83635a09e
lwwilliam <lee1052william@gmail.com>
2024-11-06 15:01:15 +0800

    add spacing in non_dir print

--- non_dir.c
+++ non_dir.c
@@ -15,7 +15,7 @@ void print_non_dir(struct s_cmd *initial_cmd)
 		for (int i = 0; initial_cmd->non_dir[i]; i++)
 		{
 			write(1, initial_cmd->non_dir[i], ft_strlen(initial_cmd->non_dir[i]));
-			write(1, " ", 1);
+			write(1, "  ", 2);
 		}
 	}
 	write(1, "\n", 1);
